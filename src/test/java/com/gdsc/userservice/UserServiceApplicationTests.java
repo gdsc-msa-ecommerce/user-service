95a8053fe41f24aac8946d7ebd20a70f540c1b40
yeonnex <yeonnex@gmail.com>
2022-12-16 13:17:38 +0900

    init

---
+++ src/test/java/com/gdsc/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
